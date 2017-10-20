7edf09f78c7f652b39dc37598d1a45416e00a32f
jharewinton <jonathan.hare-winton@guardian.co.uk>
2017-10-20 15:29:18 +0100

    add API response

--- src/main/scala/com/gu/jhwScalaLambda/Lambda.scala
+++ src/main/scala/com/gu/jhwScalaLambda/Lambda.scala
@@ -1,9 +1,18 @@
 package com.gu.jhwScalaLambda
 
+import java.io.{InputStream, OutputStream}
+
+import io.circe.syntax._
+import java.nio.charset.StandardCharsets.UTF_8
+
 object Lambda {
 
-  def handler(): String = {
-    "Hello world"
+  def handler(in: InputStream, out: OutputStream): Unit = {
+
+    val response = APIResponse(200,  Map("Content-Type" -> "application/json"), "hello world again")
+
+    //no spaces converts json to a string
+    out.write(response.asJson.noSpaces.getBytes(UTF_8))
   }
 
 }
